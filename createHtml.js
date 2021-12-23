4cc09820a4125575a12a6690eec497bdaff56c0c
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-23 17:48:57 -0500

    Code improvement

---
+++ createHtml.js
@@ -0,0 +1,93 @@
+export function displaySearches(search, showResults) {
+	const p1Class = document.createAttribute("class");
+	const p2Class = document.createAttribute("class");
+	const newDiv = document.createElement("div");
+	const h1 = document.createElement("h1");
+	const a1 = document.createElement("a");
+	const a2 = document.createElement("a");
+	const p1 = document.createElement("p");
+	const p2 = document.createElement("p");
+
+	const a1Attributes = {
+		href: `https://en.wikipedia.org/?curid=${search.pageid}`,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		class: "first-a",
+	};
+
+	const a2Attributes = {
+		href: `https://en.wikipedia.org/?curid=${search.pageid}`,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		class: "second-a",
+	};
+
+	Object.keys(a1Attributes).forEach((key) => {
+		const att = document.createAttribute(key);
+		att.value = a1Attributes[key];
+		a1.setAttributeNode(att);
+	});
+
+	Object.keys(a2Attributes).forEach((key) => {
+		const att = document.createAttribute(key);
+		att.value = a2Attributes[key];
+		a2.setAttributeNode(att);
+	});
+
+	a1.appendChild(document.createTextNode(`${search.title}`));
+	a2.appendChild(
+		document.createTextNode(`https://en.wikipedia.org/?curid=${search.pageid}`)
+	);
+
+	h1.appendChild(a1);
+
+	p1Class.value = "link";
+	p2Class.value = "paragraph";
+	p1.setAttributeNode(p1Class);
+	p2.setAttributeNode(p2Class);
+
+	p1.appendChild(a2);
+	p2.innerHTML = `${search.snippet}`;
+
+	newDiv.appendChild(h1);
+	newDiv.appendChild(p1);
+	newDiv.appendChild(p2);
+
+	showResults.appendChild(newDiv);
+}
+
+export function displayNoResults(value, showResults) {
+	const newDiv = document.createElement("div");
+	const h1 = document.createElement("h1");
+	const att = document.createAttribute("class");
+
+	att.value = "red-light";
+	h1.setAttributeNode(att);
+
+	h1.appendChild(
+		document.createTextNode(
+			`No results were found for "${value}". Please enter a valid search term.`
+		)
+	);
+
+	newDiv.appendChild(h1);
+	showResults.appendChild(newDiv);
+}
+
+export function displayFetchErr(showResults) {
+	const newDiv = document.createElement("div");
+	const h1 = document.createElement("h1");
+	const att = document.createAttribute("class");
+
+	att.value = "red-light";
+	h1.setAttributeNode(att);
+
+	h1.appendChild(
+		document.createTextNode(
+			"There was an error fetching the data, please try again."
+		)
+	);
+
+	newDiv.appendChild(h1);
+	showResults.appendChild(newDiv);
+}
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+import {
+	displaySearches,
+	displayNoResults,
+	displayFetchErr,
+} from "./createHtml.js";
+
 const showResults = document.getElementById("results-search");
 const formSubmit = document.getElementById("form-js");
 const inputBox = document.getElementById("input-js");
@@ -16,46 +22,18 @@ function fetchAndDisplay(url) {
 			return data.query.search;
 		})
 		.then((search) => {
-			let code = new String();
-
 			if (search.length == 0) {
-				code = `<div><h1 class="red-light">No results were found for "${inputBox.value}". Please enter a valid search term.</h1></div>`;
+				displayNoResults(inputBox.value, showResults);
 			} else {
-				for (i = 0; i < search.length; i++) {
-					code += `
-                <div>
-                <h1>
-					<a
-						href="https://en.wikipedia.org/?curid=${search[i].pageid}"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="first-a"
-						>${search[i].title}</a
-					>
-				</h1>
-                
-                <p class="link">
-					<a
-						href="https://en.wikipedia.org/?curid=${search[i].pageid}"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="second-a"
-						>https://en.wikipedia.org/?curid=${search[i].pageid}</a
-					>
-				</p>
-                
-                <p class="paragraph">
-					${search[i].snippet}
-				</p>
-                </div>`;
+				for (let i = 0; i < search.length; i++) {
+					displaySearches(search[i], showResults);
 				}
 			}
 
-			showResults.innerHTML = code;
 			return search;
 		})
 		.catch((error) => {
-			showResults.innerHTML = `<div><h1 class="red-light">There was an error fetching the data, please try again.</h1></div>`;
+			displayFetchErr(showResults);
 			return error;
 		})
 		.finally(() => {
@@ -67,7 +45,7 @@ function fetchAndDisplay(url) {
 function handleSubmit(e) {
 	e.preventDefault();
 	const searchTerm = encodeURIComponent(inputBox.value);
-	const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${searchTerm}&format=json&srlimit=30`;
+	const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${searchTerm}&format=json&srlimit=25`;
 	fetchAndDisplay(url);
 }
 
