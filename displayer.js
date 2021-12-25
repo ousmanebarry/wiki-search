1c131d58ee1a3ae01be3c985c11c5c397eb8bb40
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-12-25 12:11:21 -0500

    Code shortening

--- displayer.js
+++ displayer.js
@@ -1,6 +1,4 @@
 export function displaySearches(search, showResults) {
-	const p1Class = document.createAttribute("class");
-	const p2Class = document.createAttribute("class");
 	const newDiv = document.createElement("div");
 	const h1 = document.createElement("h1");
 	const a1 = document.createElement("a");
@@ -8,30 +6,21 @@ export function displaySearches(search, showResults) {
 	const p1 = document.createElement("p");
 	const p2 = document.createElement("p");
 
-	const a1Attributes = {
+	const aAttributes = {
 		href: `https://en.wikipedia.org/?curid=${search.pageid}`,
 		target: "_blank",
 		rel: "noopener noreferrer",
-		class: "first-a",
+		class: ["first-a", "second-a"],
 	};
 
-	const a2Attributes = {
-		href: `https://en.wikipedia.org/?curid=${search.pageid}`,
-		target: "_blank",
-		rel: "noopener noreferrer",
-		class: "second-a",
-	};
-
-	Object.keys(a1Attributes).forEach((key) => {
-		const att = document.createAttribute(key);
-		att.value = a1Attributes[key];
-		a1.setAttributeNode(att);
-	});
-
-	Object.keys(a2Attributes).forEach((key) => {
-		const att = document.createAttribute(key);
-		att.value = a2Attributes[key];
-		a2.setAttributeNode(att);
+	Object.keys(aAttributes).forEach((key) => {
+		if (key === "class") {
+			a1.classList.add(aAttributes[key][0]);
+			a2.classList.add(aAttributes[key][1]);
+		} else {
+			a1.setAttribute(key, aAttributes[key]);
+			a2.setAttribute(key, aAttributes[key]);
+		}
 	});
 
 	a1.appendChild(document.createTextNode(`${search.title}`));
@@ -39,30 +28,27 @@ export function displaySearches(search, showResults) {
 		document.createTextNode(`https://en.wikipedia.org/?curid=${search.pageid}`)
 	);
 
-	h1.appendChild(a1);
+	p1.classList.add("link");
+	p2.classList.add("paragraph");
 
-	p1Class.value = "link";
-	p2Class.value = "paragraph";
-	p1.setAttributeNode(p1Class);
-	p2.setAttributeNode(p2Class);
-
+	h1.appendChild(a1);		
 	p1.appendChild(a2);
 	p2.innerHTML = `${search.snippet}`;
 
-	newDiv.appendChild(h1);
-	newDiv.appendChild(p1);
-	newDiv.appendChild(p2);
+	newDiv.append(h1, p1, p2);
 
 	showResults.appendChild(newDiv);
+}
+
+export function pagesDisplay() {
+	return true;
 }
 
 export function displayErr(errType, value, showResults) {
 	const newDiv = document.createElement("div");
 	const h1 = document.createElement("h1");
-	const att = document.createAttribute("class");
 
-	att.value = "red-light";
-	h1.setAttributeNode(att);
+	h1.classList.add("red-light");
 
 	if (Boolean(errType)) {
 		h1.appendChild(
